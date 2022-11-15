f5bd189e83d8cf942af8c78d5950c69e7301450b
Sergio Kummer <sergiokummer96@gmail.com>
2022-11-15 16:40:56 -0300

    save antes da merda

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+document.querySelector( "#retrobg-sun" ).onclick = () => {
+  document.querySelector( "#retrobg" ).classList.toggle( "retrobg-shutdown" );
+};
